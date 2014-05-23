dc8e7a36faa126a23d268f1a889114b5d86e464b
Mário Areias <mario.s.s.areias@gmail.com>
2014-05-23 13:25:30 -0300

    selecting workdays on the ManuntencaoDeEfetividade page

--- src/main/java/com/thoughtworks/adp/page/ManutencaoDaEfetividadePage.java
+++ src/main/java/com/thoughtworks/adp/page/ManutencaoDaEfetividadePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,7 +20,32 @@ public class ManutencaoDaEfetividadePage {
         List<WebElement> wrongFrequencyEmployees = driver.findElements(By.xpath("//font[contains(@color,'red')]"));
 
         wrongFrequencyEmployees.get(0).findElement(By.xpath("../../..//a")).click();
-        
+
+        List<WebElement> trElements = driver.findElements(By.xpath("//table[contains(@bordercolor,'#000000')]//tbody//tr"));
+        trElements.remove(0);
+
+        List<WebElement> workdaysElements = new ArrayList<WebElement>();
+
+        for (WebElement trElement : trElements) {
+            String trElementText = trElement.getText();
+            if (isWorkDay(trElementText) && !trElementText.isEmpty() && !trElementText.contains("Afastamento")){
+                workdaysElements.add(trElement);
+            }
+        }
+
+        return;
+    }
+
+    private boolean isWorkDay(String trElementText) {
+        return !isHoliday(trElementText) && !isWeekend(trElementText);
+    }
+
+    private boolean isHoliday(String trElementText) {
+        return trElementText.contains("Feriado");
+    }
+
+    private boolean isWeekend(String trElementText) {
+        return trElementText.contains("999") && trElementText.contains("998");
     }
 
     private void switchToFrame(String bmprog) {
